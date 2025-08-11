31a11e5093df42eba1f7b524d641f4023b3bd9bb
Pedro Luka <pedrolukagt@gmail.com>
2025-08-11 17:52:59 -0300

    criando primeira pagina

--- App.js
+++ App.js
@@ -1,74 +1,137 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.container_main}>
-        <Text style={styles.title}>Testando React Native com Expo</Text>
+      <View style={styles.header}>
+        <View style={styles.logo}></View>
+        <Text style={styles.titulo_menu}>Título</Text>
       </View>
 
-      <View style={styles.container_desc}>
-        <Text style={styles.desc}>Você pode começar a desenvolver sua aplicação aqui!</Text>
-        <Text style={styles.desc}>Para mais informações, visite a documentação do Expo.</Text>
+      <View style={styles.card}>
+        <View style={styles.logo}></View>
+        
+        <View style={styles.name}>
+          <Text style={styles.titulo_card}>Card 1</Text>
+          <Text>Descrição do card 1</Text>
+        </View>
       </View>
 
-      <View>
-        <button style={styles.button}>Expo Go</button>
+      <View style={styles.card}>
+        <View style={styles.logo}></View>
+        
+        <View style={styles.name}>
+          <Text style={styles.titulo_card}>Card 2</Text>
+          <Text>Descrição do card 2</Text>
+        </View>
       </View>
 
-      <StatusBar style="auto" />
+      <View style={styles.card}>
+        <View style={styles.logo}></View>
+        
+        <View style={styles.name}>
+          <Text style={styles.titulo_card}>Card 3</Text>
+          <Text>Descrição do card 3</Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <View style={styles.logo}></View>
+        
+        <View style={styles.name}>
+          <Text style={styles.titulo_card}>Card 4</Text>
+          <Text>Descrição do card 4</Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+        <View style={styles.logo}></View>
+        
+        <View style={styles.name}>
+          <Text style={styles.titulo_card}>Card 5</Text>
+          <Text>Descrição do card 5</Text>
+        </View>
+      </View>
+
+      <View style={styles.container}>
+        <Text style={styles.botao}>Ver mais</Text>
+      </View>
+
+      <View style={styles.footer}>
+        <Text>Pedro Luka</Text>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    flexDirection: 'column',
     alignItems: 'center',
-    fontFamily: 'Noto Sans',
   },
 
-  container_main: {
-    backgroundColor: 'grey',
+  header: {
+    width: '100%',
+    height: 80,
+    backgroundColor: '#543dbbff',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+  },
+
+  logo: {
+    backgroundColor: '#fff',
+    width: 50,
+    height: 50,
+    borderRadius: 10,
+  },
+
+  titulo_menu: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  card: {
+    width: '90%',
+    backgroundColor: '#ddd',
     padding: 20,
     borderRadius: 10,
-    marginTop: 20,
-    width: '80%',
+    marginVertical: 10,
+    flexDirection: 'row',
     alignItems: 'center',
   },
 
-  title: {
-    fontSize: 26,
+  name: {
+    flex: 1,
+    marginLeft: 15,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+
+  titulo_card: {
+    fontSize: 18,
     fontWeight: 'bold',
-    color: '#000',
-    textAlign: 'center',
+    marginBottom: 5,
   },
 
-  container_desc: {
-    marginTop: 20,
-    padding: 10,
-    alignItems: 'center',
-  },
-
-  desc: {
-    fontSize: 16,
-    color: '#333',
-    textAlign: 'center',
-    marginBottom: 10,
-  },
-
-  button: {
-    backgroundColor: 'grey',
-    padding: 10,
-    borderRadius: 15,
-    marginTop: 20,
+  botao: {
+    backgroundColor: '#543dbbff',
     color: '#fff',
+    padding: 10,
+    borderRadius: 5,
     textAlign: 'center',
-    textDecoration: 'none',
-    fontSize: 16,
-    fontWeight: 'bold',
-    cursor: 'pointer',
-  }
-});
+    width: '100%',
+    marginTop: 30,
+  },
+
+  footer: {
+    width: '100%',
+    height: 100,
+    backgroundColor: '#543dbbff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
